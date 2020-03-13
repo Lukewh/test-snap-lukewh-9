bc6fda5e459a120565b9942b638aa851909a14a1
Lukewh <luke@lukewh.com>
2020-03-13 15:10:40 +0000

    Initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="test-snap-lukewh-8",
+    version="0.0.1",
+    author="Luke Wesley-Holley",
+    author_email="luke.wesley-holley@canonical.com",
+    py_modules=[],
+)
---
+++ test_snap.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+print("It's starting, v0.0.1")
+
